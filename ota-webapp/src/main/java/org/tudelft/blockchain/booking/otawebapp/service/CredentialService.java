718ded2d8684dc5bc392d38827eebc11c1a5fd5a
pumicerD <berenger.nguyen.nhon@gmail.com>
2019-01-15 13:05:10 +0100

    implemented Idemix-related methods to get : - an IdemixEnrollment - an IdemixPseudonym - an IdemixSignature

--- ota-webapp/src/main/java/org/tudelft/blockchain/booking/otawebapp/service/CredentialService.java
+++ ota-webapp/src/main/java/org/tudelft/blockchain/booking/otawebapp/service/CredentialService.java
@@ -1,15 +1,20 @@
-package org.tudelft.blockchain.booking.ca.service;
+package org.tudelft.blockchain.booking.otawebapp.service;
 
+import org.apache.milagro.amcl.FP256BN.BIG;
+import org.hyperledger.fabric.sdk.Enrollment;
+import org.hyperledger.fabric.sdk.idemix.IdemixCredential;
+import org.hyperledger.fabric.sdk.idemix.IdemixPseudonym;
+import org.hyperledger.fabric.sdk.idemix.IdemixSignature;
+import org.hyperledger.fabric.sdk.identity.IdemixEnrollment;
+import org.hyperledger.fabric_ca.sdk.HFCAClient;
+import org.hyperledger.fabric_ca.sdk.exception.EnrollmentException;
+import org.hyperledger.fabric_ca.sdk.exception.InvalidArgumentException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-import org.tudelft.blockchain.booking.ca.model.NonceResponse;
+import org.tudelft.blockchain.booking.otawebapp.model.hyperledger.HFUser;
 
-import java.util.Arrays;
+import java.net.MalformedURLException;
 
 @Component
 public class CredentialService {
@@ -24,12 +29,79 @@ public class CredentialService {
     @Autowired
     RestTemplate restTemplate;
 
-    public NonceResponse getNonceResponse() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        headers.setBasicAuth(USERNAME, PASSWORD);
-        HttpEntity<String> entity = new HttpEntity<String>("{}", headers);
+//    public NonceResponse getNonceResponse() {
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+//        headers.setBasicAuth(USERNAME, PASSWORD);
+//        HttpEntity<String> entity = new HttpEntity<String>("{}", headers);
+//
+//        return restTemplate.exchange(LOCALHOST + PORT + CREDENTIAL_ENDPOINT, HttpMethod.POST, entity, NonceResponse.class).getBody();
+//    }
 
-        return restTemplate.exchange(LOCALHOST + PORT + CREDENTIAL_ENDPOINT, HttpMethod.POST, entity, NonceResponse.class).getBody();
+    /**
+     * Get an IdemixEnrollment for an already enrolled HFUser (with a regular x509 Enrollment)
+     *
+     * @param user: existing and enrolled user
+     * @param caURL: URL to connect to the Certificate Authority server
+     * @return an IdemixEnrollment for the given user, issued by the specified CA
+     * @throws MalformedURLException
+     * @throws EnrollmentException
+     * @throws InvalidArgumentException
+     */
+    public IdemixEnrollment idemixEnroll(HFUser user, String caURL)
+        throws MalformedURLException, EnrollmentException, InvalidArgumentException {
+
+        HFCAClient hfcaClient = HFCAClient.createNewInstance(caURL, null);
+        Enrollment enrollment = hfcaClient.idemixEnroll(user.getEnrollment(), user.getMspId());
+        return (IdemixEnrollment) enrollment;
+    }
+
+    /**
+     * Create an Idemix Pseudonym for an existing IdemixEnrollment Credential
+     * @param idemixEnrollment
+     * @return
+     */
+    public IdemixPseudonym idemixPseudonym(IdemixEnrollment idemixEnrollment) {
+
+        return new IdemixPseudonym(BIG.fromBytes(idemixEnrollment.getKey().getEncoded()),
+                idemixEnrollment.getIpk());
+    }
+
+    /**
+     * Sign a given message with a Zero-Knowledge Proof of the signature for an enrolled user's specific Pseudonym
+     * @param idemixEnrollment: existing IdemixEnrollment for a user
+     * @param idemixPseudonym: existing IdemixPseudonym to sign as
+     * @param message: message to sign
+     * @return
+     */
+    public IdemixSignature idemixSignature(IdemixEnrollment idemixEnrollment,
+                                           IdemixPseudonym idemixPseudonym, byte[] message) {
+
+        IdemixCredential cred = idemixEnrollment.getCred();
+        boolean[] disclosure = new boolean[cred.getAttrs().length];
+        // disclose all attributes
+        // TODO figure out which attributes to disclose
+        for (int i = 0 ; i < disclosure.length; i++)
+            disclosure[i] = true;
+
+        return new IdemixSignature(idemixEnrollment.getCred(),
+                BIG.fromBytes(idemixEnrollment.getKey().getEncoded()),
+                idemixPseudonym,
+                idemixEnrollment.getIpk(),
+                disclosure,
+                message,
+                0,
+                idemixEnrollment.getCri());
+    }
+
+    /**
+     * Sign a given message with a Zero-Knowledge Proof of the signature for an enrolled user
+     * using a volatile Pseudonym (is not stored, cannot be re-used)
+     * @param idemixEnrollment
+     * @param message
+     * @return
+     */
+    public IdemixSignature idemixSignature(IdemixEnrollment idemixEnrollment, byte[] message) {
+        return idemixSignature(idemixEnrollment, idemixPseudonym(idemixEnrollment), message);
     }
 }
